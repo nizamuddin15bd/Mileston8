28486a6d2c67ef0488450cb7f11dac6a41b1a8d4
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-12-05 16:32:20 +0600

    destructuring

--- REST-COUNTRIES-RECAP- 44-3/js/countries.js
+++ REST-COUNTRIES-RECAP- 44-3/js/countries.js
@@ -5,18 +5,45 @@ const loadCountries = () => {
 }
 
 const displayCountries = countries => {
-    console.log(countries);
+    console.log(countries[0]);
     const countriesHTML = countries.map(country => getCountryHTML(country));
     // console.log(countriesHTML[0]);
     const container = document.getElementById('countries');
     container.innerHTML = countriesHTML.join(' ');
 }
-const getCountryHTML = country => {
+
+// oftion 2
+const getCountryHTML = ({ name, flags, area, capital, region }) => {
     return `
         <div class = "country">
-            <h2>${country.name.common}</h2>
-            <img src = "${country.flags.png}">
+            <h2>${name.common}</h2>
+            <p>Area: ${area}</p>
+            <p>Region: ${region}</p>
+            <h6>Capital: ${capital}</h6>
+            <img src = "${flags.png}">
         </div>
         `;
 }
+
+
+// oftion
+// const getCountryHTML = country => {
+//     // oftion 1
+//     const { name, flags } = country
+//     return `
+//         <div class = "country">
+//             <h2>${name.common}</h2>
+//             <img src = "${flags.png}">
+//         </div>
+//         `;
+// }
+// original
+// const getCountryHTML = country => {
+//     return `
+//         <div class = "country">
+//             <h2>${name.common}</h2>
+//             <img src = "${flags.png}">
+//         </div>
+//         `;
+// }
 loadCountries();
